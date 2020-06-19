7db5aad82505c221b2b104532217baf388a38ce6
fhernanb <fhernanb@unal.edu.co>
2020-06-19 14:34:43 -0500

    se hicieron unos cambios en los ejemplos

---
+++ Simulation/Simulando glm Bernoulli.R
@@ -0,0 +1,50 @@
+# En este ejemplo se simulan datos de un glm y se 
+# estiman los parametros del modelo
+
+# Modelo: Y~Binomial(n=1, pi) con logit(pi) = b0 + b1 * x
+# con b0=-1 y b1=1
+
+# Funcion para generar los datos
+gen_dat <- function(n, b0, b1) {
+  x <- runif(n=n)
+  eta <- b0 + b1 * x
+  pi <- exp(eta) / (1 + exp(eta))
+  y <- rbinom(n=n, size=1, prob=pi)
+  data.frame(y=y, x=x)
+}
+
+# Generando los datos
+n <- 100
+datos <- gen_dat(n=n, b0=-1, b1=1)
+head(datos)
+
+# Ajustado el modelo
+mod <- glm(y ~ x, data=datos, family=binomial(link='logit'))
+summary(mod)
+coef(mod)     # Los valores estimados son cercanos a los verdaderos
+
+# Reto --------------------------------------------------------------------
+
+# Explorar el efecto del tamano de muestra n en las estimaciones.
+# Para cada n usted debe simular 100 conjuntos de datos, estimar
+# los parametros y almacenarlos.
+
+# Aproveche el codigo de abajo y cambie xxx por la expresion correcta.
+
+n <- c(5, 10, 20, 40, 80, 160, 320, 640, 1280)
+nrep <- 100
+results <- matrix(NA, ncol=2, nrow=nrep*length(n))
+colnames(results) <- c('xxx', 'b1')
+dim(results) # para ver la dimension de la matriz donde se almacenaran
+
+for (i in n) {
+  for (j in 1:nrep) {
+    xxx <- gen_dat(n=xxx, b0=-1, b1=1)
+    mod <- glm(xxx)
+    results[xxx] <- coef(xxx)
+  }
+}
+
+# Crear un diagrama de dispersion del promedio de bi versus n.
+# Crear un diagrama de dispersion del ECM de bi versus n.
+
---
+++ Simulation/Simulando glm Normal.R
@@ -0,0 +1,51 @@
+# En este ejemplo se simulan datos de un glm y se 
+# estiman los parametros del modelo
+
+# Modelo: Y~Normal(mu, s2) con mu = 1 / (b0 + b1 * x)
+# con b0=-1 y b1=1 y s2=9
+
+# Funcion para generar los datos
+gen_dat <- function(n, b0, b1, sd) {
+  x <- runif(n=n)
+  media <- 1 / (b0 + b1 * x)
+  y <- rnorm(n=n, mean=media, sd=sd)
+  data.frame(y=y, x=x)
+}
+
+# Generando los datos
+n <- 100
+datos <- gen_dat(n=n, b0=-1, b1=1, sd=3)
+head(datos)
+
+# Ajustado el modelo
+mod <- glm(y ~ x, data=datos, family=gaussian(link='inverse'))
+summary(mod)
+coef(mod)     # Los valores estimados son cercanos a los verdaderos
+
+# Reto --------------------------------------------------------------------
+
+# Explorar el efecto del tamano de muestra n en las estimaciones.
+# Para cada n usted debe simular 100 conjuntos de datos, estimar
+# los parametros y almacenarlos.
+
+# Aproveche el codigo de abajo y cambie xxx por la expresion correcta.
+
+n <- c(5, 10, 20, 40, 80, 160, 320, 640, 1280)
+nrep <- 100
+results <- matrix(NA, ncol=3, nrow=nrep*length(n))
+colnames(results) <- c('xxx', 'b1', 'sigma2')
+dim(results) # para ver la dimension de la matriz donde se almacenaran
+
+for (i in n) {
+  for (j in 1:nrep) {
+    xxx <- gen_dat(n=xxx, b0=-1, b1=1)
+    mod <- glm(xxx)
+    results[xxx] <- xxx
+  }
+}
+
+# Crear un diagrama de dispersion del promedio de bi versus n.
+# Crear un diagrama de dispersion del promedio de sigma2 versus n.
+# Crear un diagrama de dispersion del ECM de bi versus n.
+# Crear un diagrama de dispersion del ECM de sigma2 versus n.
+
