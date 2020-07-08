7260111225a22355e92602156bec92bbc12c8585
fhernanb <fhernanb@unal.edu.co>
2020-07-08 17:37:23 -0500

    se mejoró el ejemplo

--- 09 Hypothesis testing/Wald test para glm que NO tienen parametro de dispersion.R
+++ 09 Hypothesis testing/Wald test para glm que NO tienen parametro de dispersion.R
@@ -9,14 +9,14 @@ Crabs <- read.table(url, header=TRUE)
 # Y=1 si la cangreja SI tiene satelites pegados
 
 # El objetivo es estudiar las siguientes hipotesis
-# H0: beta_width  = -15
-# H1: beta_width != -15
+# H0: beta_width  = 0.3
+# H1: beta_width != 0.3
 
 fit <- glm(y ~ width, family=binomial, data=Crabs)
 summary(fit)
 
 # Calculando el estadistico y su valor-P
-z <- (-12.3508 - (-15)) / 2.6287
+z <- (0.4972 - 0.3) / 0.1017
 z
 2 * pnorm(q=z, lower.tail=FALSE) # valor-P
 
@@ -28,6 +28,18 @@ z
 library(usefultools)
 shadow.dist(dist='dnorm', param=list(mean=0, sd=1),
             a=-z, b=z, type='two', from=-3, to=3)
+
+# Otra forma de hacer la prueba es usando el paquete multcomp
+# Revisar la url de abajo para conocer los detalles
+# https://fhernanb.github.io/libro_regresion/ph2.html#comparaciones-m%C3%BAltiples
+
+library(multcomp)
+C <- matrix(c(0, 1), ncol=2, byrow=TRUE)
+mult_test <- glht(model=fit, linfct=C, 
+                  alternative='two.sided', 
+                  rhs=c(0.3))
+summary(mult_test, test = adjusted(type="single-step"))
+
 
 # Ejemplo 2 - Poisson -----------------------------------------------------
 
@@ -43,7 +55,8 @@ Crabs$color <- as.factor(Crabs$color)
 Crabs$spine <- as.factor(Crabs$spine)
 
 # Se desea modelar la variable Y que representa el 
-# numero de satelites.
+# numero de satelites en funcion de
+# weight, color, width y spine
 
 # El objetivo es estudiar las siguientes hipotesis
 # H0: beta_weight = 0.1
@@ -66,3 +79,64 @@ pnorm(q=z, lower.tail=FALSE) # valor-P
 library(usefultools)
 shadow.dist(dist='dnorm', param=list(mean=0, sd=1),
             a=z, type='upper', from=1, to=4)
+
+# Otra forma de hacer la prueba es usando el paquete multcomp
+# Revisar la url de abajo para conocer los detalles
+# https://fhernanb.github.io/libro_regresion/ph2.html#comparaciones-m%C3%BAltiples
+
+library(multcomp)
+C <- matrix(c(0, 1, 0, 0, 0, 0, 0, 0), ncol=8, byrow=TRUE)
+mult_test <- glht(model=fit, linfct=C, 
+                  alternative='greater', 
+                  rhs=c(0.1))
+summary(mult_test, test = adjusted(type="single-step"))
+
+# Ejemplo 3 - Poisson -----------------------------------------------------
+
+# Usando los datos de cangrejos
+# color: 1, medium light; 2, medium; 3, medium dark; 4, dark 
+# spine condition: 1, both good; 2, one worn or broken; 3, both worn or broken
+# Width, carapace width (cm); 
+# Weight, weight (kg).
+
+url <- "http://users.stat.ufl.edu/~aa/glm/data/Crabs.dat"
+Crabs <- read.table(url, header=TRUE)
+Crabs$color <- as.factor(Crabs$color)
+Crabs$spine <- as.factor(Crabs$spine)
+
+# Se desea modelar la variable Y que representa el 
+# numero de satelites en funcion de
+# weight, color, width y spine
+
+# El objetivo es estudiar las siguientes hipotesis
+# H0: (beta_weight, beta_width)  = (0, 0)
+# H1: (beta_weight, beta_width) != (0, 0)
+
+fit <- glm(y ~ weight + color + width + spine, 
+           family=poisson(link=log), data=Crabs)
+summary(fit)
+
+# Los betas estimados
+coef(fit)
+# Todos los betas en matriz columna
+beta_hat <- matrix(coef(fit), ncol=1,
+                   dimnames=list(names(coef(fit)), NULL))
+beta_hat
+# Matriz L
+L <- matrix(c(0, 1, 0, 0, 0, 0, 0, 0,
+              0, 0, 0, 0, 0, 1, 0, 0), ncol=8, byrow=TRUE)
+L
+L %*% beta_hat # subvector con los betas en H0
+
+# Matriz C
+C <- matrix(c(0, 0), ncol=1, byrow=TRUE)
+C
+
+# Estadistico
+aux <- L %*% beta_hat - C
+z2 <- t(aux) %*% solve(L %*% vcov(fit) %*% t(L)) %*% aux
+z2 <- as.numeric(z2)
+z2
+pchisq(q=z2, df=2, lower.tail=FALSE) # valor-P
+
+
--- 09 Hypothesis testing/Wald test para glm que SI tienen parametro de dispersion.R
+++ 09 Hypothesis testing/Wald test para glm que SI tienen parametro de dispersion.R
@@ -19,6 +19,7 @@ fit <- glm(price ~ size + new + beds,
            data=datos, family=Gamma(link=inverse))
 summary(fit)
 
+# Mean Deviance estimator for phi
 deviance(fit) / df.residual(fit)
 
 # La nueva tabla de resumen es
@@ -38,3 +39,59 @@ library(usefultools)
 shadow.dist(dist='dt', param=list(df=100-4),
             a=T, type='lower', from=-5, to=-4)
 
+# Conclusion: como el valor-P es menor que 5% hay evidencias
+# para rechazar H0, es decir, hay evidencias para pensar
+# que beta_size < -0.000001
+
+# Ejemplo 2 - Gamma -------------------------------------------------------
+
+# Usando los datos de las casas
+url <- 'http://users.stat.ufl.edu/~aa/glm/data/Houses.dat'
+datos <- read.table(url, header=TRUE)
+head(datos, n=5)
+
+# Se desea modelar la variable Price=eta usando un glm con 
+# respueta gamma, funcion de enlace inversa y size, new y beds.
+
+# El objetivo es estudiar las siguientes hipotesis
+# H0: (beta_new, beta_beds) = c(0, 0)
+# H1: (beta_new, beta_beds) != c(0, 0)
+
+# Se ajustan los modelos
+fit <- glm(price ~ size + new + beds,
+           data=datos, family=Gamma(link=inverse))
+summary(fit)
+
+# Los betas estimados
+coef(fit)
+# Todos los betas en matriz columna
+beta_hat <- matrix(coef(fit), ncol=1,
+                   dimnames=list(names(coef(fit)), NULL))
+beta_hat
+# Matriz L
+L <- matrix(c(0, 0, 1, 0,
+              0, 0, 0, 1), ncol=4, byrow=TRUE)
+L
+L %*% beta_hat # subvector con los betas en H0
+
+# Matriz C
+C <- matrix(c(0, 0), ncol=1, byrow=TRUE)
+C
+
+# Maximum likelihood estimate
+MASS::gamma.dispersion(fit)
+
+# Matriz de varianza y covarianzas
+var_cov_hat <- vcov(summary(fit, dispersion=0.1328696))
+var_cov_hat
+
+# Estadistico
+aux <- L %*% beta_hat - C
+F0 <- t(aux) %*% solve(L %*% var_cov_hat %*% t(L)) %*% aux
+F0 <- as.numeric(F0)
+F0
+pf(q=F0, df1=2, df2=100-4, lower.tail=FALSE) # valor-P
+
+# Conclusion: como el valor-P es menor que 5% se concluye que hay 
+# evidencias para rechazar H0, es decir, hay evidencias para 
+# pensar que (beta_new, beta_beds) != c(0, 0)
