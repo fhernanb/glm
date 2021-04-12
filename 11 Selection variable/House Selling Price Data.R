11a1f5bdcd799c0a08b2376755ab8bc2cba76ee6
fhernanb <fhernanb@unal.edu.co>
2021-04-12 10:59:16 -0500

    se agregó un grafico en ggplot2

--- 11 Selection variable/House Selling Price Data.R
+++ 11 Selection variable/House Selling Price Data.R
@@ -15,6 +15,21 @@ url <- 'http://users.stat.ufl.edu/~aa/glm/data/Houses.dat'
 datos <- read.table(url, header=TRUE)
 head(datos, n=5)
 
+# Vamos a explorar la relacion entre el precio con las otras variables explicativas
+library(ggplot2)
+library(patchwork)
+
+p1 <- ggplot(datos) + geom_point(aes(size, price), alpha=0.2)
+p2 <- ggplot(datos) + geom_point(aes(taxes, price), alpha=0.2)
+p3 <- ggplot(datos, aes(x=as.factor(baths), y=price)) + 
+  geom_boxplot(fill="slateblue", alpha=0.2) + 
+  xlab("Baths")
+p4 <- ggplot(datos, aes(x=as.factor(beds), y=price)) + 
+  geom_boxplot(fill="slateblue", alpha=0.2) + 
+  xlab("Beds")
+
+(p1 + p2) / (p3 + p4)
+
 # Vamos a ajustar un naive model
 naive_mod <- glm(price ~ 1, data=datos, family=Gamma(link="inverse"))
 summary(naive_mod)
@@ -23,11 +38,11 @@ summary(naive_mod)
 mean(datos$price)
 1 / mean(datos$price)
 
-
 # Backward del paquete MASS
 full_form <- formula(price ~ beds + baths + size + taxes + new +
                        I(beds^2) + I(baths) + I(taxes)+
                        beds * new + baths * new + size * new)
+
 full_mod <- glm(price ~ beds + baths + size + taxes + new +
                   I(beds^2) + I(baths) + I(taxes)+
                   beds * new + baths * new + size * new, 
