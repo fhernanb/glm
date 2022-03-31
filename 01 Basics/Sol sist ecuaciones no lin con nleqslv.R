1b475f0af78da45f9fdf3b0c5e175553e0342c08
fhernanb <fhernanb@unal.edu.co>
2022-03-31 11:05:51 -0500

    se agregó un ejemplo de ecuaciones de estimacion para Poisson

--- 01 Basics/Sol sist ecuaciones no lin con nleqslv.R
+++ 01 Basics/Sol sist ecuaciones no lin con nleqslv.R
@@ -1,3 +1,6 @@
+
+# Ejemplo 1 ---------------------------------------------------------------
+
 # El ejemplo de abajo corresponde al ejemplo de la ayuda de la
 # funcion nleqslv del paquete nleqslv
 
@@ -24,4 +27,45 @@ nleqslv(x=xstart,
 
 # comprobando
 fun(c(1, 1))
+
+
+# Ejemplo 2 ---------------------------------------------------------------
+
+# El ejemplo de abajo corresponde al ejemplo mostrado en
+# https://math.libretexts.org/Bookshelves/Algebra/Intermediate_Algebra_(OpenStax)/11%3A_Conics/11.06%3A_Solving_Systems_of_Nonlinear_Equations
+
+fun <- function(x) {
+  y <- numeric(2)
+  y[1] <-  x[1]   - x[2] + 2
+  y[2] <- -x[1]^2 + x[2]
+  y
+}
+
+# Una prueba iniciando de los valores x1=2 y x2=0.5
+xstart <- c(2, 0.5)
+fun(xstart)
+
+# a solution is c(1,1)
+library(nleqslv)
+nleqslv(x=xstart, 
+        fn=fun, 
+        method="Broyden",
+        control=list(btol=0.01))
+
+# comprobando
+fun(c(2, 4))
+
+# Otra prueba iniciando de los valores x1=-3 y x2=1
+xstart <- c(-3, 1)
+fun(xstart)
+
+# a solution is c(1,1)
+library(nleqslv)
+nleqslv(x=xstart, 
+        fn=fun, 
+        method="Broyden",
+        control=list(btol=0.01))
+
+# comprobando
+fun(c(-1, 1))
 
---
+++ 02 Estimation/Estimacion con ecua vero Poisson.R
@@ -0,0 +1,58 @@
+# En este ejemplo se solucionan las ecuaciones de verosimilitud
+# de un glm Poisson
+
+# El enunciado de este ejercicio esta en la pag 125 del libro, reto 1
+
+
+# Equation ----------------------------------------------------------------
+fun1 <- function(betas) {
+  b0 <- betas[1]
+  b1 <- betas[2]
+  z <- numeric(2) # contiene LD de las ecuaciones
+  z[1] <- (4 - exp(b0+5*b1))   + (3 - exp(b0+4*b1))   + (1 - exp(b0+3*b1))
+  z[2] <- (4 - exp(b0+5*b1))*5 + (3 - exp(b0+4*b1))*4 + (1 - exp(b0+3*b1))*3
+  z
+}
+
+# Exploremos fun
+fun1(betas=c(1, 5))   # upss, queriamos obtener (0, 0)
+fun1(betas=c(-1, -5)) # upss, queriamos obtener (0, 0)
+fun1(betas=c(-1, 2))  # upss, queriamos obtener (0, 0)
+
+# Ejemplo con 3 datos -----------------------------------------------------
+y <- c(4, 3, 1)
+x <- c(5, 4, 3)
+
+library(nleqslv)
+res <- nleqslv(x=c(0, 0), fn=fun1, method="Newton",
+               control=list(btol=0.01))
+res
+
+
+
+# Super Reto --------------------------------------------------------------
+
+# Como se podria modificar la funcion fun1 para que use muestras con mas obs
+# pero sin tener que escribir termino a termino en z[1] y z[2]
+
+# Cambiar ___ por la instruccion correcta
+
+y <- c(4, 3, 1)
+x <- c(5, 4, 3)
+
+fun2 <- function(betas) {
+  b0 <- betas[1]
+  b1 <- betas[2]
+  z <- numeric(2)
+  mu <- exp(b0 + b1 * x)
+  z[1] <- sum( (___ - mu) * ___ )
+  z[2] <- sum( (___ - mu) * ___ )
+  z
+}
+
+library(nleqslv)
+res <- nleqslv(x=c(0, 0), fn=fun2, method="Newton",
+               control=list(btol=0.01))
+res
+
+
